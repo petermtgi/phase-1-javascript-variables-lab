fcadb236e35ffe26ca33701bdebd06b6e50ce6e3
Peter Mutungi <3sjk0krrr@gmail.com>
2025-03-17 13:08:24 +0300

    js-variables-lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// Declare companyName as a constant with value 'Scuber'
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo using let
+let companyCeo = 'Susan Smith';
